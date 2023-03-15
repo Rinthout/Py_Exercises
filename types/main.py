40c7434c1495a794924870e440e031ee01aa67d8
Rinthout <95086904+Rinthout@users.noreply.github.com>
2023-03-15 11:01:23 +0100

    Add files via upload

---
+++ types/main.py
@@ -0,0 +1,13 @@
+# Exercise types
+print(type(3))
+print(type(3.14))
+print(type('Pi'))
+
+some_number = 5
+print(type(some_number))
+
+another_number = 9.81
+print(type(another_number))
+
+some_string = 'Hello world'
+print(type(some_string))
